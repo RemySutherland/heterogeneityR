f209fd87847621b78f52b8a21f2c38d95135d9e1
Remy Sutherland <remy.sutherland@okstate.edu>
2025-03-04 14:11:35 -0600

    Update_commit

--- R/rfm_test.R
+++ R/rfm_test.R
@@ -1,7 +1,7 @@
 #' Returns error and convergence estimates for Random Forest Model based on training data
 #'
 #' @param training Training data file for Random Forest Model
-#' @param class Column within training data file denoting focal vegetation
+#' @param focal_column Column within training data file denoting focal vegetation
 #' @param thresh Numeric value of threshold value for Random Forest prediction
 #' @param ntree Number of trees to grow for Random Forest Model
 #' @param mtry Number of variables to randomly sample as candidates at each split
@@ -9,11 +9,15 @@
 #' @return A print summary of estimated model error and plot of error as a function number of trees
 #' @export
 
-rfm_test <- function(training,class,thresh,ntree,mtry){
+rfm_test <- function(training,focal_column,thresh,ntree,mtry){
 palette_selection_veg<- training
-veg_class <- class
+focal_column <- deparse(substitute(focal_column))
 palette_selection_veg <- dplyr::group_by(palette_selection_veg,mix)
-veg.rfm<- suppressWarnings(randomForest::randomForest(veg_class~red+green+blue,data=palette_selection_veg, ntree=ntree,mtry = mtry,importance=TRUE))
-print(veg.rfm)
-plot(veg.rfm)
+palette_selection_veg<- dplyr::ungroup(palette_selection_veg)
+
+formula<- reformulate(termlabels = c("red", "green", "blue"), response = focal_column)
+
+class.rfm<- suppressWarnings(randomForest::randomForest(formula,data=palette_selection_veg, ntree=ntree,mtry = mtry,importance=TRUE))
+print(class.rfm)
+plot(class.rfm)
 }
