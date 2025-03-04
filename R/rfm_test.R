ce63aa79d1b9b612efdb03746fc9e77fa4cbd5e1
Remy Sutherland <remy.sutherland@okstate.edu>
2025-03-04 13:04:11 -0600

    Add rfm_test() function

---
+++ R/rfm_test.R
@@ -0,0 +1,19 @@
+#' Returns error and convergence estimates for Random Forest Model based on training data
+#'
+#' @param training Training data file for Random Forest Model
+#' @param class Column within training data file denoting focal vegetation
+#' @param thresh Numeric value of threshold value for Random Forest prediction
+#' @param ntree Number of trees to grow for Random Forest Model
+#' @param mtry Number of variables to randomly sample as candidates at each split
+#'
+#' @return A print summary of estimated model error and plot of error as a function number of trees
+#' @export
+
+rfm_test <- function(training,class,thresh,ntree,mtry){
+palette_selection_veg<- training
+veg_class <- class
+palette_selection_veg <- dplyr::group_by(palette_selection_veg,mix)
+veg.rfm<- suppressWarnings(randomForest::randomForest(veg_class~red+green+blue,data=palette_selection_veg, ntree=ntree,mtry = mtry,importance=TRUE))
+print(veg.rfm)
+plot(veg.rfm)
+}
